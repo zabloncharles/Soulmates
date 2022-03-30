994deb86b468d160096c1f272be0b5a507d22606
Fusionlion <74889517+Fusionlion@users.noreply.github.com>
2022-03-30 12:52:27 -0400

    Initial Commit

---
+++ ionicFusion/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ionicFusion
+//
+//  Created by Zablon Charles on 3/30/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ionicFusion/ionicFusionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ionicFusionApp.swift
+//  ionicFusion
+//
+//  Created by Zablon Charles on 3/30/22.
+//
+
+import SwiftUI
+
+@main
+struct ionicFusionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
