7d8ec694fa27f5ee31f328e6bc0d13f9255880b4
Zablon Charles <zablon.charles@gmail.com>
2024-04-14 15:23:55 -0500

    Initial Commit

---
+++ Soul25/Soul25App.swift
@@ -0,0 +1,17 @@
+//
+//  Soul25App.swift
+//  Soul25
+//
+//  Created by Zablon Charles on 4/14/24.
+//
+
+import SwiftUI
+
+@main
+struct Soul25App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
